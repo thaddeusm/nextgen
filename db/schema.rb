1f5fc3332695eb0dea3b65001d3e88222e586e48
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2016-09-28 22:10:21 +0300

    changed branding and landing styles

--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20160906150203) do
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "bootsy_image_galleries", force: :cascade do |t|
     t.string   "bootsy_resource_type"
     t.integer  "bootsy_resource_id"
@@ -50,9 +33,7 @@ ActiveRecord::Schema.define(version: 20160906150203) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "user_id"
-    t.string   "ancestry"
     t.string   "focus"
-    t.index ["ancestry"], name: "index_feeds_on_ancestry"
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
@@ -84,6 +65,16 @@ ActiveRecord::Schema.define(version: 20160906150203) do
     t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "writings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "feed_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feed_id"], name: "index_writings_on_feed_id"
+    t.index ["user_id"], name: "index_writings_on_user_id"
   end
 
 end
