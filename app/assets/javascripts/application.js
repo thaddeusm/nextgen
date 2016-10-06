54b3591fc061b134d2f4de12d2bb21926473a04c
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2016-10-06 19:46:28 +0300

    edited navbar backgrounds across screen sizes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,26 +21,3 @@ $(document).ready(function() {
     setTimeout(function() {
         $(".alert").alert('close');
     }, 4000);
-$('nav').removeClass('dropback');
-
-    $("#navCol").on("click", function() {
-        $('nav').addClass('dropback');
-    });
-
-
-            // hide .navbar first
-        $("nav").removeClass('navback');
-            // fade in .navbar
-        $(function () {
-            $(window).scroll(function() {
-                
-                // set distance
-                if ($(this).scrollTop() > 100) {
-                    $('nav').addClass('navback');
-                } else {
-                    $('nav').removeClass('navback');
-                }
-                });
-            });
-        
-        });
